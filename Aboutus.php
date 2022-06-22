9fe8d7048fc6a522e20e1ea0d7f222c1a11001d6
Rhileilanto <leah79675@gmail.com>
2022-06-22 14:03:59 +0300

    assignment-004-php-database

---
+++ Aboutus.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap-5.2.0-beta1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="Aboutus.html">
+    <title>Document</title>
+</head>
+<body>
+        <!--Navbar starts here-->
+        <nav class="navbar navbar-expand-lg bg-light fixed-top shadow">
+            <div class="container-fluid">
+                <a href="index.html" class="navbar-brand">Zalego academy</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" aria-expanded="false" data-bs-target="#menus">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="menus">
+                    <div class="navbar-nav">
+                        <a href="index.html" class="nav-link active">Home</a>
+                        <a href="Aboutus.html" class="nav-link">About Us</a>
+                        <a href="#" class="nav-link">Contact us</a>
+                    </div>
+                </div>
+            </div>
+    
+        </nav>
+    
+        <!--Navbar ends-->
+        <main class="p-5 bg-light mb-4 ">
+            <h1>About Us</h1>
+            <p>This is a template for a simple marketing or informational website. It includes a large callout a jumbtron and three supporting pieces of content. Use it as astarting point to create something more unique.</p>
+        </main>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6">
+                    <h1>Our Programs</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus vitae sed voluptatibus amet similique laborum.</p>
+                    <img src="Images/Download.jpg" alt="Elearning">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <h1>The Programs</h1> 
+                <div class="col-lg-4">
+                    <div class="card">
+                        <div class="card-body">
+                    <h2>Skill Discovery</h2>
+                    <p>Fusce dapibus, tellus ac cursus cammodo, tortor. Fuiid cofjng</p>
+                    <button class="btn btn-primary">View Details</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="card">
+                        <div class="card-body">
+                    <h2>Upskilling Program</h2>
+                    <p>Fusce dapibus, tellus ac cursus cammodo, tortor. Fuiid cofjng</p>
+                    <button class="btn btn-primary">View Details</button>
+                    <span class="border-left"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="card">
+                        <div class="card-body">
+                    <h2>Path finding Program</h2>
+                    <p>Fusce dapibus, tellus ac cursus cammodo, tortor. Fuiid cofjng</p>
+                    <button class="btn btn-primary">View Details</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <p>Subscribe to get information, latest news about Zalego Academy</p>
+                <form>
+                    <div class="row">
+                       <div class="mb-3 col-lg-6 col-md-6">
+                            <input type="email" class="form-control" placeholder="Your email address">
+                        </div>
+                        <div class="mb-3 col-lg-6 col-md-6">
+                             <button class="btn btn-primary">Subscribe</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        <script src="bootstrap-5.2.0-beta1-dist/js/bootstrap.bundle.min.js.map"></script>
+        <!--<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>-->
+       <script src="bootstrap-5.2.0-beta1-dist/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,148 @@
+<?php
+$server="localhost";
+$username="root";
+$password="";
+$database="web2";
+
+$conn=mysqli_connect($server,$username,$password,$database)
+
+if( isset($_POST[submitButton]))
+{
+    $insertData = mysqli_query($conn, "INSERT INTO
+    subscriber(no,email,created_at)
+   VALUES('$no','$email','created_at')");
+
+
+}
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap-5.2.0-beta1-dist/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <!--Navbar starts here-->
+    <nav class="navbar navbar-expand-lg bg-light fixed-top shadow">
+        <div class="container-fluid">
+            <a href="index.html" class="navbar-brand">Zalego academy</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" aria-expanded="false" data-bs-target="#menus">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="menus">
+                <div class="navbar-nav">
+                    <a href="index.html" class="nav-link active">Home</a>
+                    <a href="Aboutus.html" class="nav-link">About Us</a>
+                    <a href="#" class="nav-link">Contact us</a>
+                </div>
+            </div>
+        </div>
+
+    </nav>
+
+    <!--Navbar ends-->
+
+    <main class="p-5 bg-light mb-4 ">
+        <h1 style="padding-top: 80px;">Welcome, Wanjiru Leah</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis ullam, adipisci voluptas reprehenderit minima eum, nihil iste distinctio iusto nulla harum debitis. Consectetur rerum fugiat, deserunt alias porro, ullam, reiciendis eius veniam tempora quibusdam quo.</p>
+       <button class="btn btn-primary">Learn More</button>
+    </main>
+    <div class="container">
+        <div class="row ">
+            <div class="col-lg-4 col-md-6">
+                <h1>Header 1</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dignissimos excepturi 
+                    voluptas delectus adipisci labore doloremque, voluptatem provident tempore incidunt,
+                     obcaecati nam et fuga distinctio officiis officia est asperiores doloribus?
+                </p>
+                <button class="btn btn-primary">View Details</button>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <h1>Header 2</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dignissimos excepturi 
+                    voluptas delectus adipisci labore doloremque, voluptatem provident tempore incidunt,
+                     obcaecati nam et fuga distinctio officiis officia est asperiores doloribus?
+                </p>
+                <button class="btn btn-primary">View Details</button>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <h1>Header 3</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dignissimos excepturi 
+                    voluptas delectus adipisci labore doloremque, voluptatem provident tempore incidunt,
+                     obcaecati nam et fuga distinctio officiis officia est asperiores doloribus?
+                </p>
+                <button class="btn btn-primary">View Details</button>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <h1>Header 4</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dignissimos excepturi 
+                    voluptas delectus adipisci labore doloremque, voluptatem provident tempore incidunt,
+                     obcaecati nam et fuga distinctio officiis officia est asperiores doloribus?
+                </p>
+                <button class="btn btn-primary">View Details</button>
+            </div>
+           <form action="index.php" method="post"></form>
+            
+        </div>
+        <!--Contact Us starts here-->
+        <div class="row pt-5">
+            <h1>Contact Us</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore odit exercitationem soluta dolor nisi quo consequuntur nostrum iste, 
+                similique officiis quas sapiente repellendus! Minus repellendus rem soluta neque consequatur minima.
+            </p>
+            <form>
+                <div class="row">
+                    <div class="mb-3 col-lg-6 col-md-6 col-sm-12">
+                        <label for="firstname" class="form-lebel">First Name</label>
+                        <input type="text" name="firstname" class="form-control" placeholder="Enter Your First Name">
+                    </div>
+                    <div class="mb-3 col-lg-6 col-md-6 col-sm-12">
+                        <label for="lastname" class="form-lebel">Last Name</label>
+                        <input type="text" name="lastname" class="form-control" placeholder="Enter Your Last Name">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="mb-3 col-lg-6 col-md-6 col-sm-12">
+                        <label for="PhoneNumber" class="form-lebel">First Name</label>
+                        <input type="tel" class="form-control" placeholder="Enter Your Phone Number">
+                    </div>
+                    <div class="mb-3 col-lg-6 col-md-6 col-sm-12">
+                        <label for="Email" class="form-lebel">Email</label>
+                        <input type="email" name="email"class="form-control" placeholder="Enter Email Address">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="mb-3 col-lg-12">
+                        <label for="message" class="form-label">Your message</label>
+                        <textarea cols="30" rows="5" class="form-control"></textarea>
+                    </div>
+                </div>
+                <button type="submit" name="submitButton" class="btn btn-primary">Send Message</button>
+            </form>
+        </div>
+
+        <!--End of Contact Us-->
+
+        <hr>
+        <footer>
+            &COPY;Zalego Academy 2022
+        </footer>
+    </div>
+    
+        
+    <script src="bootstrap-5.2.0-beta1-dist/js/bootstrap.bundle.min.js.map"></script>
+     <!--<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>-->
+    <script src="bootstrap-5.2.0-beta1-dist/js/bootstrap.min.js"></script>
+</body>
+</html>
