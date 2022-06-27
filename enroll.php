6c97b5e76e7e02714e1466db1c71ae4d2f8eabab
Rhileilanto <leah79675@gmail.com>
2022-06-27 14:04:38 +0300

    assignment-004-php-database

--- enroll.php
+++ enroll.php
@@ -12,22 +12,21 @@
 
 
          <!--Navbar starts here-->
-         <nav class="navbar navbar-expand-lg bg-light fixed-top shadow">
-            <div class="container-fluid">
-                <a href="index.html" class="navbar-brand">Zalego academy</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" aria-expanded="false" data-bs-target="#menus">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="menus">
-                    <div class="navbar-nav">
-                        <a href="index.html" class="nav-link active">Home</a>
-                        <a href="Aboutus.html" class="nav-link">About Us</a>
-                        <a href="#" class="nav-link" class="btn btn-primary">Register now</a>
-                    </div>
+    <nav class="navbar navbar-expand-lg bg-light fixed-top shadow">
+        <div class="container-fluid">
+            <a href="index.html" class="navbar-brand">Zalego academy</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" aria-expanded="false" data-bs-target="#menus">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="menus">
+                <div class="navbar-nav">
+                    <a href="index.html" class="nav-link active">Home</a>
+                    <a href="Aboutus.html" class="nav-link">About Us</a>
+                    <a href="enroll.php"class="btn btn-primary" class="nav-link">Register now</a>
                 </div>
             </div>
-    
-        </nav>
+        </div>
+    </nav>
         
         <!--Navbar ends-->
         <main class="p-5 bg-light mb-4 ">
@@ -37,30 +36,53 @@
         <h4>Devan Plaza</h4>
         <form>
         <div class="row">
-                    <div class="mb-3 col-lg-6 col-md-6 col-sm-12">
-                        <label for="fullname" class="form-lebel">Full Name:</label>
+                    <div class="mb-3 col-lg-6 col-md-6 col-sm-12 ">
+                        <label for="fullname" class="form-lebel">Full Name: <Address></Address></label>
                         <input type="text" name="fullname" class="form-control" placeholder="Enter Your Full Name">
                     </div>
-                    <div class="mb-3 col-lg-6 col-md-6 col-sm-12">
+                    <div class="mb-3 col-lg-6 col-md-6 col-sm-12  ">
                         <label for="Emailaddress" class="form-lebel">Email Address: <Address></Address></label>
                         <input type="email" name="email" class="form-control" placeholder="Please enter your email">
                     </div>
                 </div>
                 <div class="row">
                     <div class="mb-3 col-lg-6 col-md-6 col-sm-12">
-                        <label for="PhoneNumber" class="form-lebel">Phone Number:</label>
+                        <label for="PhoneNumber" class="form-lebel">Phone Number: <Address></Address></label>
                         <input type="tel" name="phonenumber" class="form-control" placeholder="+2547.....">
                     </div>
-                    <div class="mb-3 col-lg-6 col-md-6 col-sm-12">
-                        <label for="Whats your gender" class="form-lebel">What's your gender:</label>
-                        <input type="text" name="gender" class="form-control" placeholder="--Select your gender--">
+                    <div class="mb-3 col-lg-6  col-md-6 col-sm-12 ">
+                        <label for="Whats your gender" class="form-lebel">What's your gender: <Address></Address></label>
+                        <select class="form-select" aria-lebel="default select example">
+                            <option selected>--select your gender--</option>
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                        </select>
                     </div>
                 </div>
                 
                 </div>
                 <p class="padding-top: 50px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis voluptas ab cumque, dolorum quis nulla assumenda debitis libero at, magnam tenetur sequi ex illo id et recusandae autem. Deserunt, natus.</p>
         </form>
-
+        <div class="row">
+            <div class="mb-3 col-lg-6 col-lg-12">
+                <label for="course" class="form-lebel">What is preferred course ? <Address></Address></label>
+                <select class="form-select" aria-lebel="default select example">
+                    <option selected>--select your course--</option>
+                    <option value="Standard">Standard</option>
+                    <option value="Advanced">Advanced</option>
+                    <option value="Diploma">Diploma</option>
+                </select>    
+           
+        </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum non blanditiis suscipit sint atque vero? Fugit mollitia tempora ex nobis placeat dolores omnis amet aperiam aliquam ad ipsa, odit at.</p>
+    
+        <div class="row">
+        
+                <input type="text" class="form-control" placeholder="">
+                <label for="conditions" class="form-lebel">Agree terms and conditions</label>
+                
+        </div> 
+        <button class="btn btn-primary">Submit application</button>
         <script src="bootstrap-5.2.0-beta1-dist/js/bootstrap.bundle.min.js.map"></script>
         <!--<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>-->
        <script src="bootstrap-5.2.0-beta1-dist/js/bootstrap.min.js"></script>
